37be0a635ee83b8f38a64767309325006414b7fa
Vijayakumar <vijayakumardpr@gmail.com>
2022-08-23 07:35:43 +0530

    Add files via upload

--- register.js
+++ register.js
@@ -1,39 +1,52 @@
 const formRegister = document.getElementById("form-register")
+const emailError = document.getElementById("email-error-notes")
 
-let localData = JSON.parse(localStorage.getItem("registerData")) || []
+//let localData = JSON.parse(localStorage.getItem("registerData")) || []
 
+let allInformation = JSON.parse(localStorage.getItem("registerData")) || []
 
-let allInformation = []
+formRegister.addEventListener("submit", () => {
+  let fistName = document.getElementById("firstName")
+  let lastName = document.getElementById("lastName")
+  let email = document.getElementById("email")
+  let password = document.getElementById("password")
 
-formRegister.addEventListener("submit", ()=>{
-    let fistName = document.getElementById("firstName")
-    let lastName = document.getElementById("lastName")
-    let email = document.getElementById("email")
-    let password = document.getElementById("password")
+  if (
+    firstName.value === "" &&
+    lastName.value === "" &&
+    email.value === "" &&
+    password.value === ""
+  )
+    return
 
-    if(firstName.value === "" && lastName.value === "" && email.value==="" && password.value==="") return 
-
-    let personData = {
-        firstName: fistName.value,
-        lastName: lastName.value,
-        email:email.value,
-        password: password.value
+  allInformation.forEach((data) => {
+    if (email.value === data.email) {
+      emailError.style.display = "block"
+      emailError.textContent = "Email already registered"
+      return
     }
+    return
+  })
 
+  let personData = {
+    firstName: fistName.value,
+    lastName: lastName.value,
+    email: email.value,
+    password: password.value,
+  }
 
-    allInformation.push(personData)
+  allInformation.push(personData)
 
-    
-    localStorage.setItem("registerData", JSON.stringify(allInformation))
+  console.log(personData)
 
+  localStorage.setItem("registerData", JSON.stringify(allInformation))
 
-    
-    
-    firstName.value= ""
-    lastName.value=""
-    email.value=""
-    password.value=""
-
-
-
+  clearForm()
 })
+
+function clearForm() {
+  firstName.value = ""
+  lastName.value = ""
+  email.value = ""
+  password.value = ""
+}
--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 const form = document.querySelector("#form-login")
+const pwdError = document.getElementById("password-error-note")
 
 const allData = JSON.parse(localStorage.getItem("registerData")) || []
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const userName = document.getElementById("login-username")
+  const password = document.getElementById("login-password")
 
-allData.forEach(data =>{
-    console.log(data.firstName);
+  if (userName.value === "" && password.value === "") return
+
+  allData.forEach((data) => {
+    if (userName.value === data.firstName) {
+      if (password.value !== data.password) {
+        pwdError.style.display = "block"
+        pwdError.textContent = "Password is incorrect"
+      } else {
+        window.location = "content.html"
+        clearForm()
+      }
+    } else if (
+      userName.value === data.firstName &&
+      password.value === data.password
+    ) {
+      window.location = "content.html"
+      clearForm()
+    } else {
+      alert("You new here register first")
+      window.location = "index.html"
+      clearForm()
+    }
+  })
 })
 
-
-
-form.addEventListener("submit", (e)=>{
-    e.preventDefault()
-    const userName = document.getElementById("login-username")
-    const password = document.getElementById("login-password")
-
-    if(userName.value === "" && password.value === "") return 
-
-        allData.forEach(data => {
-        if(userName.value === data.firstName && password.value === data.password){
-            window.location = "content.html"
-        }else{
-            //alert("Your are new here register first ")
-            window.location = "index.html"
-        }
-
-    }) 
-
-    userName.value = ""
-    password.value = ""
-
-    
-})
+function clearForm() {
+  userName.value = ""
+  password.value = ""
+}
